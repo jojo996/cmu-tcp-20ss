5e9f562f8003fded891d21e6545084decf391cd5
marphownio <20300290032@fudan.edu.cn>
2022-12-29 23:31:10 -0800

    save modification

--- project-2_15-441/inc/cmu_packet.h
+++ project-2_15-441/inc/cmu_packet.h
@@ -30,6 +30,27 @@
 #include "grading.h"
 
 typedef struct {
+	uint32_t identifier;   		//4 bytes
+	uint16_t source_port;		//2 bytes
+	uint16_t destination_port;	//2 bytes
+	uint32_t seq_num; 			//4 bytes
+	uint32_t ack_num; 			//4 bytes
+	uint16_t hlen;				//2 bytes //header length
+	uint16_t plen;				//2 bytes //packet length
+	uint8_t flags;				//1 byte
+	uint16_t advertised_window; //2 bytes
+	uint16_t extension_length;  //2 bytes
+	char* extension_data;	    //X bytes
+} cmu_header_t;
+
+
+typedef struct {
+	cmu_header_t header;
+	struct timeval sent_time;
+	char* data;
+} cmu_packet_t;
+
+typedef struct {
   uint32_t identifier;         // 4 Identifier for the CMU-TCP protocol.
   uint16_t source_port;        // 2 Source port.
   uint16_t destination_port;   // 2 Destination port.
@@ -79,6 +100,7 @@ void set_advertised_window(cmu_tcp_header_t* header,
 void set_extension_length(cmu_tcp_header_t* header, uint16_t extension_length);
 void set_extension_data(cmu_tcp_header_t* header, uint8_t* extension_data);
 void free_packet(cmu_packet_t* packet);
+char* packet_to_buf(cmu_packet_t* p);
 
 /**
  * Sets all the header fields.
@@ -155,25 +177,9 @@ char* create_packet_buf(uint16_t src, uint16_t dst, uint32_t seq, uint32_t ack,
     uint16_t hlen, uint16_t plen, uint8_t flags, uint16_t adv_window, 
     uint16_t ext, char* ext_data, char* data, int len);
 
-typedef struct {
-	uint32_t identifier;   		//4 bytes
-	uint16_t source_port;		//2 bytes
-	uint16_t destination_port;	//2 bytes
-	uint32_t seq_num; 			//4 bytes
-	uint32_t ack_num; 			//4 bytes
-	uint16_t hlen;				//2 bytes //header length
-	uint16_t plen;				//2 bytes //packet length
-	uint8_t flags;				//1 byte
-	uint16_t advertised_window; //2 bytes
-	uint16_t extension_length;  //2 bytes
-	char* extension_data;	    //X bytes
-} cmu_header_t;
 
-typedef struct {
-	cmu_header_t header;
-	struct timeval sent_time;
-	char* data;
-} cmu_packet_t;
+
+
 /**
  * Checks if a given sequence number comes before another sequence number.
  *
